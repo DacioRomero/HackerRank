ee91311173f5c05cb34ce0f95c6bb6e4c87f5404
FireClaw <Dacio_Romero@outlook.com>
2016-10-27 17:00:07 -0400

    Formatted "Substring.java"

--- Java/src/com/hackerrank/java/strings/Substring.java
+++ Java/src/com/hackerrank/java/strings/Substring.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Substring {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		String string = scanner.next();
 		int start = scanner.nextInt();
 		int end = scanner.nextInt();
-		
+
 		scanner.close();
-		
+
 		System.out.println(string.substring(start, end));
 	}
 }
