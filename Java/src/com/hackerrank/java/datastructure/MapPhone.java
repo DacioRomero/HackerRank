f83f789b27f9e6ac9fd058978a69d240d48af1ed
FireClaw <Dacio_Romero@outlook.com>
2016-10-27 16:10:23 -0400

    Renamed files to be consistent with HackerRank and added progress
    Renamed:
    - "Array1DPart1.java" to "Subarray.java"
    - "PhoneMap.java" to "MapPhone.java" for consistency
    
    Finished new Data Structures->Java 1D Array

--- Java/src/com/hackerrank/java/datastructure/MapPhone.java
+++ Java/src/com/hackerrank/java/datastructure/MapPhone.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class PhoneMap {
+public class MapPhone {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
