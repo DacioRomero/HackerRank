ca02220d8d0cacbb9ee391e1f2a57ef4c1613073
FireClaw <Dacio_Romero@outlook.com>
2016-10-27 15:41:05 -0400

    Renamed files
    Renamed:
    - Removed "String" from Strings subdomain
    - "Token.java" to "Tokens.java"

---
+++ Java/src/com/hackerrank/java/strings/Reverse.java
@@ -0,0 +1,15 @@
+package com.hackerrank.java.strings;
+
+import java.util.Scanner;
+
+public class Reverse {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		StringBuilder string = new StringBuilder(scanner.next().toUpperCase());
+
+		scanner.close();
+
+		System.out.println(string.toString().equals(string.reverse().toString()) ? "Yes" : "No");
+	}
+}
