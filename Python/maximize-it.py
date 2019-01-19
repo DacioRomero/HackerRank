08edc673dd539ca08fbe53407b1315f215a35392
Dacio <dacioromero@gmail.com>
2019-01-19 12:10:50 -0800

    Update maximize-it.py

--- Python/maximize-it.py
+++ Python/maximize-it.py
@@ -1,5 +1,13 @@
-if __name__ == '__main__':
-    K, M = map(int, input().split())
+import itertools
 
-    Ni = [list(map(int, input()))[1:] for _ in range(K)]
-    print(Ni)
+def main():
+    list_count, divisor = list(map(int, input().split(' ')))
+
+    lists = (list(map(int, input().split()))[1:] for _ in range(list_count))
+
+    results = (sum(map(lambda x: x * x, l)) % divisor for l in itertools.product(*lists))
+    
+    print(max(results))
+
+if __name__ == '__main__':
+    main()
