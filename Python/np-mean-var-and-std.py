3a83dd60c6d3180d4c3ca4391247451bf6f4f95c
Dacio <dacioromero@gmail.com>
2019-01-22 15:34:06 -0800

    Update np-mean-var-and-std.py

--- Python/np-mean-var-and-std.py
+++ Python/np-mean-var-and-std.py
@@ -1,7 +1,7 @@
 import numpy
 
 def main():
-    _, my_array = input(), numpy.array([list(map(int, input().split())) for _ in range(2)])
+    my_array = numpy.array([list(map(int, input().split())) for _ in range(int(input().split()[0]))])
     numpy.set_printoptions(legacy='1.13')
     print(numpy.mean(my_array, axis=1))
     print(numpy.var(my_array, axis=0))
